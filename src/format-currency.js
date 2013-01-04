1b88ad6fd7ebf04989a189d08862187bf9abc12e
Jason Edwards <jtanium@gmail.com>
2013-01-04 10:01:37 -0700

    The negative sign is now at the beginning of the formatted string.

--- src/format-currency.js
+++ src/format-currency.js
@@ -2,9 +2,16 @@ function numberToCurrency(number) {
 
     var currencySymbol     = '$';
     var thousandsSeparator = ',';
+    var numberIsNegative   = false;
 
     number = stripDollarSign(number);
     number = isNaN(number) || number == '' || number == null ? 0.00 : number;
+
+    if (number < 0) { 
+        numberIsNegative = true;   // indicate we are working with a negative number
+        number = Math.abs(number); // "remove" the negative sign so we can put it where we want
+    }
+
     var numberStr = parseFloat(number).toFixed(2).toString();
     var numberFormatted = new Array(numberStr.slice(-3));   // this returns the decimal and cents
     numberStr = numberStr.substring(0, numberStr.length-3); // this removes the decimal and cents
@@ -19,6 +26,7 @@ function numberToCurrency(number) {
     }
     numberFormatted.unshift(numberStr); // there are less than three digits in numberStr, so prepend them
     numberFormatted.unshift(currencySymbol); // prepend the currencySymbol
+    if (numberIsNegative) { numberFormatted.unshift("-"); } // prepend a "-" when the number is negative
 
     return numberFormatted.join(''); // put it all together
 }
