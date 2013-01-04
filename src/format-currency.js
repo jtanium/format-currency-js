3b8a7d2573f854f808b5e2d9d1ec97db1cdc1254
Jason Edwards <jtanium@gmail.com>
2013-01-04 15:24:07 -0700

    Added options to customize output.

--- src/format-currency.js
+++ src/format-currency.js
@@ -1,37 +1,36 @@
-function numberToCurrency(number) {
+function numberToCurrency(number, options) {
+    // set defaults...
+    if (typeof options == 'undefined') { options = {}; }
+    var precision = options.precision || 2
+    var unit      = options.unit      || '$'
+    var separator = options.separator || '.'
+    var delimiter = options.delimiter || ','
+    var format    = options.format    || '%u%n'
+    var negativeFormat = options.negativeFormat || '-%u%n'
 
-    var currencySymbol     = '$';
-    var thousandsSeparator = ',';
-    var numberIsNegative   = false;
+    // "clean up" number
+    if (typeof number == 'string') { number = number.replace(/\$/g, ''); } // strip dollar sign
+    number = isNaN(number) || number == '' || number == null ? 0.0 : number; // set to 0.0 if we can't tell what it is
 
-    number = stripDollarSign(number);
-    number = isNaN(number) || number == '' || number == null ? 0.00 : number;
-
+    // determine which format to use
     if (number < 0) { 
-        numberIsNegative = true;   // indicate we are working with a negative number
-        number = Math.abs(number); // "remove" the negative sign so we can put it where we want
+        format = negativeFormat;
+        number = Math.abs(number); // "remove" the negative sign
     }
 
-    var numberStr = parseFloat(number).toFixed(2).toString();
-    var numberFormatted = new Array(numberStr.slice(-3));   // this returns the decimal and cents
-    numberStr = numberStr.substring(0, numberStr.length-3); // this removes the decimal and cents
-    /*
-     * Why is there an `unshift()` function, but no `shift()`?
-     * Also, a `pop()` function would be handy here.
-     */
+    // 'separate' the cents
+    var numberStr = parseFloat(number).toFixed(precision).toString();
+    var numberFormatted = new Array(numberStr.slice(-1*precision));   // this returns the cents
+    numberFormatted.unshift(separator); // add the separator
+    numberStr = numberStr.substring(0, numberStr.length-(precision+1)); // this removes the decimal and cents
+
+    // 'delimit' the thousands
     while (numberStr.length > 3) {
         numberFormatted.unshift(numberStr.slice(-3)); // this prepends the last three digits to `numberFormatted`
-        numberFormatted.unshift(thousandsSeparator); // this prepends the thousandsSeparator to `numberFormatted`
+        numberFormatted.unshift(delimiter); // this prepends the delimiter to `numberFormatted`
         numberStr = numberStr.substring(0, numberStr.length-3);  // this removes the last three digits
     }
     numberFormatted.unshift(numberStr); // there are less than three digits in numberStr, so prepend them
-    numberFormatted.unshift(currencySymbol); // prepend the currencySymbol
-    if (numberIsNegative) { numberFormatted.unshift("-"); } // prepend a "-" when the number is negative
 
-    return numberFormatted.join(''); // put it all together
-}
-
-function stripDollarSign(s) {
-    if (typeof s == 'string') { s = s.replace(/\$/g, ''); }
-    return s;
+    return format.gsub(/%u/,unit).gsub(/%n/,numberFormatted.join('')); // put it all together
 }
